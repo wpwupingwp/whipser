e00ee46fb6687fcc9bfcb8bac07b305606e1bbc1
Ping Wu <wpwupingwp@outlook.com>
2023-04-27 21:20:43 +0800

    add url

--- run_whisper.sh
+++ run_whisper.sh
@@ -1,2 +1,3 @@
+# https://github.com/ggerganov/whisper.cpp
 ffmpeg -i [fielname] -acodec pcm_s16le -ac 1 -ar 16000 out.wav
 ./main -m models/ggml-large.bin -l zh -threads 6 -olrc -ovtt -f [filename]
